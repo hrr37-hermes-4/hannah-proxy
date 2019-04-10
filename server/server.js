8e0e4e81047f3029514d4b113498adb99b99e7f0
Hannah Kim <hannahsohkim@gmail.com>
2019-04-09 21:59:13 -0700

    Create proxy server and route to different services

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+
+app.use('/books/:id', express.static(path.join(__dirname, '/../public')));
+
+
+app.use('/books/:id/info', proxy({ target: 'http://localhost:3002', changeOrigin: true }));
+
+app.use('/books/:id/reviews', proxy({ target: 'http://localhost:3003', changeOrigin: true }));
+
+app.use('/books/:id/details', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
+
+app.use('/books/:id/authors', proxy({ target: 'http://127.0.0.1:3000', changeOrigin: true }));
+
+
+const PORT = 3005;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+});
